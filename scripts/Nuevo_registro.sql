39e14d2bc90d3396ea0af2694592279f2746315b
chapid611 <nenfarchapid@gmail.com>
2024-09-11 13:55:34 -0500

    11/09/2024

---
+++ scripts/Nuevo_registro.sql
@@ -0,0 +1,25 @@
+
+insert into users(name, last_name, mail)
+values("Nenfar","Chapid", "nenfar@gmail.com"),
+("Juan","Lopez", "juan@gmail.com"),
+("Andrea", "Suarez","andrea@gmail.com"),
+("Camilo", "Cortez","camilo@gmail.com"),
+("Maria", "Torres","maria@gmail.com"),
+("Esteban", "Rodriguez","esteban@gmail.com");
+select * from languages;
+insert into languages(name_language, community, description)
+values("Awapit","Awá","Awapit es la lengua hablada por la comunidad Awá"),
+("A'ingae","Confán","A'ingae es la lengua hablada por la comunidad Cofán"),
+("Koreguaje","Coreguaje","Koreguaje es la lengua hablada por la comunidad Coreguaje"),
+("Embera","Embera","Embera es la lengua hablada por la comunidad Embera"),
+("Inga","Inga","Inga es la lengua hablada por la comunidad Inga, que pertenece a la familia Quechua"),
+("Kamentsá","Kamentsá","kamentsá es la lengua hablada por la comunidad Kamentsá"),
+("Kichwa","Kichwa","Kichwa es la lengua hablada por la comunidad Kichwa, que es una variante del Quechua"),
+("Nasa Yuwe","Nasa","Nasa Yuwe es la lengua hablada por la comunidad Nasa"),
+("Pasto","Pastos","Pasto es la lengua hablada por la comunidad Pastos"),
+("Siona","Siona","Siona es la lengua hablada por la comunidad Siona"),
+("Murui Muinane","Murui Muina","Murui Muinane es la lengua hablada por la comunidad Murui Muina"),
+("Pijao","Pijao","Pijao es la lengua hablada por la comunidad Pijao"),
+("Carijona","Carijona","Carijona es la lengua hablada por la comunidad Carijona")
+
+
